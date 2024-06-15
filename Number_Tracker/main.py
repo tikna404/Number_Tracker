058e9120be36f5a2199442679d3b4f271618c0a7
Ankit Pal <ankitp2104@gmail.com>
2024-06-15 15:00:56 +0530

    main.py

--- Number_Tracker/main.py
+++ Number_Tracker/main.py
@@ -1,7 +1,7 @@
 import phonenumbers
 from phonenumbers import timezone, geocoder, carrier
 
-number = input("enter the number:: ")
+number = input("enter the number:: ") # enter the number with the country code
 phone = phonenumbers.parse(number)
 time = timezone.time_zones_for_number(phone)
 sim_details = carrier.name_for_number(phone,"en")
